82da4aca7794722f2a41a51591da404c45bda9e4
Reid Vanvliet <Reidvanvliet56@hotmail.com>
2025-07-07 22:56:53 -0700

    timeout for animation

--- src/components/home/ScrollAnimation.js
+++ src/components/home/ScrollAnimation.js
@@ -4,27 +4,23 @@ import { useEffect, useDeferredValue } from "react";
 
 gsap.registerPlugin(ScrollTrigger);
 
-
 const ScrollAnimation = ({ children, identifier, side, divName }) => {
-    useEffect(() => {
-    gsap.to('.' + identifier, {
-      transform: "translate(0, 0)", // Moves the section horizontally
-      ease: "none",
-      immediateRender: false,
-      scrollTrigger: {
-        trigger: '.' + identifier,
-        start: "top bottom",     // starts when top of section hits bottom of viewport
-        end: "bottom 70%",       // ends when bottom hits top
-        scrub: true,         
-      }
-    });
+  useEffect(() => {
+    setTimeout(() => {
+      gsap.to("." + identifier, {
+        transform: "translate(0, 0)", // Moves the section horizontally
+        ease: "none",
+        scrollTrigger: {
+          trigger: "." + identifier,
+          start: "top bottom", // starts when top of section hits bottom of viewport
+          end: "bottom 70%", // ends when bottom hits top
+          scrub: true,
+        },
+      });
+    }, 1000);
   }, []);
 
-  return (
-    <div className={`${divName} ${side} ${identifier}`}>
-      { children }
-    </div>
-  )
- }
+  return <div className={`${divName} ${side} ${identifier}`}>{children}</div>;
+};
 
- export default ScrollAnimation;
+export default ScrollAnimation;
